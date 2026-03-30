dc73e46c2d780962f060e5faa3d5760de977583c
Tharun-Kumar-McW <tharun.kumar@multicorewareinc.com>
2026-03-30 08:56:54 +0530

    Custom ops for matmul with native and tiled

--- Mojo_custom_ops/kernels/matmul.mojo
+++ Mojo_custom_ops/kernels/matmul.mojo
@@ -4,7 +4,7 @@ from tensor import InputTensor, OutputTensor
 from std.utils.index import IndexList
 from std.algorithm import parallelize
 
-@compiler.register("matmultiply")
+@compiler.register("matmultiply-native")
 struct MatMul:
     @staticmethod
     def execute[
@@ -22,8 +22,7 @@ struct MatMul:
         fn matmul_cal() capturing: #capturing is used to access variables from the outer scope
             for m in range(M):
                 for col in range(N):
-                    var acc = SIMD[output.dtype, 1](0.0)
-                    
+                    var acc = SIMD[output.dtype, 1](0)
                     for k in range(K):
                         var av = a.load[1](IndexList[2](m, k))
                         var bv = b.load[1](IndexList[2](k, col))
@@ -31,3 +30,66 @@ struct MatMul:
                         
                     output.store[1](IndexList[2](m, col), acc)
         matmul_cal()
+
+@compiler.register("matmultiply-vectorized")
+struct MatMulVectorized:
+    @staticmethod
+    def execute[
+        target: StaticString,
+    ](
+        output: OutputTensor,
+        a: InputTensor[dtype=output.dtype, rank=2, ...],
+        b: InputTensor[dtype=output.dtype, rank=2, ...],
+        ctx: DeviceContextPtr,
+    ) raises:
+        var M = output.dim_size(0)
+        var N = output.dim_size(1)
+        var K = a.dim_size(1)
+        comptime M_TILE = 16
+        comptime N_TILE = 16
+        comptime K_TILE = 16
+        comptime TILE = 16
+
+        # fn matmul_tiled() capturing:
+
+        #     # Outer tile loops — select which tile to work on
+        #     for m_tile in range(0, M, TILE):           # steps: 0, 16, 32, 48
+        #         for n_tile in range(0, N, TILE):       # steps: 0, 16, 32, 48
+        #             for k_tile in range(0, K, TILE):   # steps: 0, 16, 32, 48
+
+        #                 var m_end = min(m_tile + TILE, M)   # boundary guard
+        #                 var n_end = min(n_tile + TILE, N)   # boundary guard
+        #                 var k_end = min(k_tile + TILE, K)   # boundary guard
+
+        #                 for m in range(m_tile, m_end):
+        #                     for col in range(n_tile, n_end):
+        #                         var acc = SIMD[output.dtype, 1](0.0)
+        #                         for k in range(k_tile, k_end):
+        #                             var av = a.load[1](IndexList[2](m, k))
+        #                             var bv = b.load[1](IndexList[2](k, col))
+        #                             acc += av * bv
+                                    
+        #                         output.store[1](IndexList[2](m, col), acc)
+        fn matmul_tiled() capturing:
+            var out_ptr = output.unsafe_ptr()
+            for m_tile in range(0, M, TILE):           
+                for n_tile in range(0, N, TILE):       
+                    var m_end = min(m_tile + TILE, M)   
+                    var n_end = min(n_tile + TILE, N)   
+                    for m in range(m_tile, m_end):
+                        for col in range(n_tile, n_end):
+                            out_ptr.store(m * N + col, SIMD[output.dtype, 1](0))
+                    for k_tile in range(0, K, TILE):   
+                        var k_end = min(k_tile + TILE, K)  
+                        for m in range(m_tile, m_end):
+                            for col in range(n_tile, n_end):
+                                var acc = SIMD[output.dtype, 1](0)
+                                for k in range(k_tile, k_end):
+                                    var av = a.load[1](IndexList[2](m, k))
+                                    var bv = b.load[1](IndexList[2](k, col))
+                                    acc += av * bv
+                                var flat_idx = m * N + col
+                                var existing_val = out_ptr.load(flat_idx)
+                                out_ptr.store(flat_idx, existing_val + acc)
+
+        matmul_tiled()
--- Mojo_custom_ops/matmul_test.py
+++ Mojo_custom_ops/matmul_test.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import time
 import numpy as np
 from max.driver import CPU, Accelerator, Buffer, accelerator_count
 from max.dtype import DType
@@ -9,68 +9,85 @@ from max.graph import DeviceRef, Graph, TensorType, ops
 if __name__ == "__main__":
     mojo_kernels = Path("./kernels")
     # mxk kxn => mxn
-    m = 4
-    n = 3
-    k = 3
-    dtype = DType.float32
+    m = 64
+    n = 64
+    k = 64
+    dtype = DType.int32
     device = CPU() if accelerator_count() == 0 else Accelerator()
     print(f"Using device: {device}")
     # Configure our simple one-operation graph.
-    graph = Graph(
-        "matmultiplication",
-        forward=lambda a, b: ops.custom(
-            name="matmultiply",
-            device=DeviceRef.from_device(device),
-            values=[a, b],
-            out_types=[
+
+    def func(kernel_name):
+        print(f"Testing kernel: {kernel_name}")
+        graph = Graph(
+            "matmultiplication",
+            forward=lambda a, b: ops.custom(
+                name=kernel_name,
+                device=DeviceRef.from_device(device),
+                values=[a, b],
+                out_types=[
+                    TensorType(
+                        dtype=a.dtype,
+                        shape=[m, n],
+                        device=DeviceRef.from_device(device),
+                    )
+                ],
+            )[0].tensor,
+            input_types=[
                 TensorType(
-                    dtype=a.dtype,
-                    shape=[m, n],
+                    dtype,
+                    shape=[m, k],
                     device=DeviceRef.from_device(device),
-                )
+                ),
+                TensorType(
+                    dtype,
+                    shape=[k, n],
+                    device=DeviceRef.from_device(device),
+                ),
             ],
-        )[0].tensor,
-        input_types=[
-            TensorType(
-                dtype,
-                shape=[m, k],
-                device=DeviceRef.from_device(device),
-            ),
-            TensorType(
-                dtype,
-                shape=[k, n],
-                device=DeviceRef.from_device(device),
-            ),
-        ],
-        custom_extensions=[mojo_kernels],
-    )
+            custom_extensions=[mojo_kernels],
+        )
 
-    session = InferenceSession(
-        devices=[device],
-    )
+        session = InferenceSession(
+            devices=[device],
+        )
 
-    model = session.load(graph)
+        model = session.load(graph)
+        a_mat = np.random.uniform(low=1, high=5, size=(m, k)).astype(np.int32)
+        b_mat = np.random.uniform(low=1, high=5, size=(k, n)).astype(np.int32)
 
-    # Fill an input matrix with random values.
-    a_mat = np.random.uniform(low=1.0, high=2.0, size=(m, k)).astype(np.float32)
-    b_mat = np.random.uniform(low=1.0, high=2.0, size=(k, n)).astype(np.float32)
+        a = Buffer.from_numpy(a_mat).to(device)
+        b = Buffer.from_numpy(b_mat).to(device)
 
-    # Create tensors and move them to the device (CPU or GPU).
-    a = Buffer.from_numpy(a_mat).to(device)
-    b = Buffer.from_numpy(b_mat).to(device)
+        # Run inference with the input tensor.
+        result = model.execute(a, b)[0]
 
-    # Run inference with the input tensor.
-    result = model.execute(a, b)[0]
+        start = time.time()
+        result = model.execute(a, b)[0]
+        end = time.time()
+        print(f"Inference time: {end - start:.6f} seconds for kernel {kernel_name}")
 
-    # Copy values back to the CPU to be read.
-    assert isinstance(result, Buffer)
-    result = result.to(CPU())
+        assert isinstance(result, Buffer)
+        result = result.to(CPU())
+        # return result.to_numpy()
 
-    print("--- Original Input ---\nA matrix:")
-    print(a_mat)
-    print("\nB matrix:")
-    print(b_mat)
-    print("\n--- Graph result (Mojo MatMul) ---")
-    print(result.to_numpy())
-    print("\n--- Expected result (NumPy MatMul) ---")
-    print(a_mat @ b_mat)
+        print("--- Original Input ---\nA matrix:")
+        print(a_mat)
+        print("\nB matrix:")
+        print(b_mat)
+        print(f"\n--- Graph result (Mojo MatMul) {kernel_name} ---")
+        print(result.to_numpy())
+        print("\n--- Expected result (NumPy MatMul) ---")
+        print(a_mat @ b_mat)
+    
+    func("matmultiply-native")
+    func("matmultiply-vectorized")
+    # print("\n--- Actual NumPy matmul result for reference ---")
+    # print(a_mat @ b_mat)
+    # print("\n--- Verifying results between kernels ---")
+    # print("Result from matmultiply-native:")
+    # print(res1)
+    # print("\nResult from matmultiply-vectorized:")
+    # print(res2)
+    # np.testing.assert_allclose(res1, res2, rtol=1e-5, err_msg="Kernels produced different math!")
+    # print("Math verified! Both kernels match.")
